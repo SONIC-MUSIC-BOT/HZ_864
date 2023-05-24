e8a1d9cac08bdda76fa627cdd6022dc4599989ce
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-05-24 15:03:04 +0300

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid, user_id):
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (450, 25),
+                (455, 25),
                 f"SONIC SOURCE",
                 fill="white",
                 stroke_width=3,
